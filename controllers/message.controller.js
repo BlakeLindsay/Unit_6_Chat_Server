7237f3e62b768184af137a36a788f70cbc21d21c
BlakeLindsay <104230616+BlakeLindsay@users.noreply.github.com>
2023-10-05 19:03:33 -0600

    update message working

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -2,8 +2,14 @@ const router = require("express").Router();
 const Room = require("../models/room.model");
 const Message = require("../models/message.model");
 const validateSession = require('../middleware/validateSession');
+const { validate } = require("../models/user.model");
 
-
+/**
+ * gives a standard error
+ * 
+ * @param {*} res 
+ * @param {*} error 
+ */
 function errorResponse(res, error) {
 	res.status(500).json({
 		ERROR: error.message
@@ -48,6 +54,25 @@ router.get("/:room", async (req, res) => {
 		:
 		res.status(404).json({message: "no messages found"});
 
+	} catch (error) {
+		errorResponse(res, error);
+	}
+});
+
+router.patch("/", validateSession, async (req, res) => {
+	try {
+		let info = {
+			date: Date(),
+			text: req.body.text
+		};
+		let updatedMessage = await Message.findOneAndUpdate({_id: req.body.id, owner: req.user.id}, info, {new: true});
+		if (!updatedMessage) {
+			throw new Error("Invalid Message / User combination");
+		}
+		res.status(200).json({
+			message: `${req.body.id} updated`,
+			text: req.body.text
+		});
 	} catch (error) {
 		errorResponse(res, error);
 	}
