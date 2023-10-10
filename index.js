0dfe159e93ac16df4492d43f56942438c7ec146c
BlakeLindsay <104230616+BlakeLindsay@users.noreply.github.com>
2023-10-09 20:33:53 -0600

    Update index.js

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ db.once('open', () => console.log(`Connected to: ${MONGO}`));
 app.use(express.json());
 
 const users = require('./controllers/user.controller');
-//const rooms = require('./controllers/room.controller');
 const rooms = require('./controllers/room.controller');
-//const messages = require('./controllers/message.controller');
+const rooms = require('./controllers/room.controller');
+const messages = require('./controllers/message.controller');
 
 app.use('/user', users);
 app.use('/room', rooms);
